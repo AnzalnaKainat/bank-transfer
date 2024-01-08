ee1485473d4f2a469e6c3e3d669f4338f0d2d4ec
AnzalnaKainat <anzalnakainat@gmail.com>
2024-01-08 21:53:31 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+function makeTransaction() {
+    window.location.href = "transaction.html";
+}
+
+function redirectToHistory() {
+    window.location.href = "history.html";
+}
+function backToHomePage(){
+    window.location.href = "index.html";
+}
+function BackToMoneyTransfer(){
+    window.location.href = "money-transfer.html";
+}
+
+function toggleMenu() {
+    var sidebar = document.querySelector('.sidebar');
+    sidebar.classList.toggle('show-sidebar');
+}
